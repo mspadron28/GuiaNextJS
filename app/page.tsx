4357720e3fb7dcf7ada47b7da8f203d85612d2c7
mspadron28 <matiaspadron2@gmail.com>
2024-09-28 23:02:32 -0500

    Cambios de la guia hasta chapter 10

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        {<AcmeLogo /> }
         <div className={styles.shape}></div>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -24,7 +24,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
